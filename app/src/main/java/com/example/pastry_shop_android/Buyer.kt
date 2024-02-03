ce376f5fc9704736545918f79e88bb5df466e284
Andrija Stojanović <stojanovicandrija123@gmail.com>
2024-02-04 00:07:06 +0100

    Added header as component and added routing between activities

--- app/src/main/java/com/example/pastry_shop_android/Buyer.kt
+++ app/src/main/java/com/example/pastry_shop_android/Buyer.kt
@@ -1,12 +1,29 @@
 package com.example.pastry_shop_android
 
 import android.os.Bundle
+import android.view.Gravity.RIGHT
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 
 class Buyer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.log_in)
+        setContentView(R.layout.buyer)
+
+        val dropdown_menu = findViewById<ImageView>(R.id.dropdown_menu)
+        dropdown_menu.setOnClickListener {
+            val popUpMenu = PopupMenu(this, dropdown_menu)
+            popUpMenu.inflate(R.menu.popup_menu)
+            popUpMenu.setOnMenuItemClickListener{
+                Toast.makeText(this, "Item: " + it.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+            popUpMenu.show()
+        }
     }
 }
